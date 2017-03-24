175fdebe7d77626940be6c4ddf79611564830d69
Segi Manzanares <segifredo.manzanares@copyleft.com.mx>
2017-03-24 13:50:39 -0600

    Init website plugins

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,18 @@
-console.log("Hola");
-console.log("Mundo");
+$(document).ready(function () {
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.button-collapse').sideNav({'edge': 'left'});
+    if (isTouchDevice()) {
+        $('#nav-mobile').css({overflow: 'auto'});
+    }
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+});
+// Detect touch screen and enable scrollbar if necessary
+function isTouchDevice() {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
